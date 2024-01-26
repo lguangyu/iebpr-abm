7cded556d6ec93ad8892a434dac1c530db9ef066
Guangyu Li <gl343@cornell.edu>
2024-01-26 13:40:43 -0500

    fix rela_count reduction in split

--- src/agent_subtype_base.cpp
+++ src/agent_subtype_base.cpp
@@ -24,9 +24,9 @@ namespace iebpr
 
 	using agent_itr_t = AgentSubtypeBase::agent_itr_t;
 
-	void _sort_by_biomass(agent_itr_t itrs[2]) noexcept
+	void _sort_biomass_ascend(agent_itr_t itrs[2]) noexcept
 	{
-		if (itrs[0]->state.s.biomass <= itrs[1]->state.s.biomass)
+		if (itrs[0]->state.s.biomass > itrs[1]->state.s.biomass)
 			return;
 		std::swap(itrs[0], itrs[1]);
 		return;
@@ -75,29 +75,31 @@ namespace iebpr
 		if (n_agent <= 1)
 			return;
 
-		// update the splitting agent state, except split_biomass
-		auto t = agent_itr->state.s.split_biomass;
+		// update the splitting agent state, except split_biomass and rela_count
+		auto sb = agent_itr->state.s.split_biomass;
+		auto rc = agent_itr->state.s.rela_count;
 		agent_itr->state.scale_state_content(0.5);
-		agent_itr->state.s.split_biomass = t;
+		agent_itr->state.s.split_biomass = sb;
+		agent_itr->state.s.rela_count = rc;
 
 		// copy the state, will be the splitted agent state
 		AgentState split_state = agent_itr->state;
 
 		// find the two agents with lowest biomass and merge
 		// when reaching here we have at least two agents
-		agent_itr_t merge_itrs[2] = {pool_begin(), pool_begin() + 1};
-		_sort_by_biomass(merge_itrs);
+		agent_itr_t to_merge_itrs[2] = {pool_begin(), pool_begin() + 1};
+		_sort_biomass_ascend(to_merge_itrs);
 		for (agent_itr_t itr = pool_begin() + 2; itr < pool_end(); itr++)
-			if (itr->state.s.biomass < merge_itrs[1]->state.s.biomass)
+			if (itr->state.s.biomass < to_merge_itrs[1]->state.s.biomass)
 			{
-				merge_itrs[1] = itr;
-				_sort_by_biomass(merge_itrs);
+				to_merge_itrs[1] = itr;
+				_sort_biomass_ascend(to_merge_itrs);
 			}
 		// merge the two, the merged one is used for the new split agent
-		merge_itrs[1]->merge_with(*merge_itrs[0]);
-		merge_itrs[0]->state = split_state;
+		to_merge_itrs[1]->merge_with(*to_merge_itrs[0]);
+		to_merge_itrs[0]->state = split_state;
 		// trait of the new split will be randomized (approximate mutation (?))
-		_randomize_agent_trait(merge_itrs[0]->trait);
+		_randomize_agent_trait(to_merge_itrs[0]->trait);
 		return;
 	}
 
